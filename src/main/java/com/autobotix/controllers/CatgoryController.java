03bd001bd0828665fa6eb2ea8dbfb673bc3fc91b
Krishna <sid.kumarroyal@gmail.com>
2022-02-13 16:43:56 +0530

    test

--- src/main/java/com/autobotix/controllers/CatgoryController.java
+++ src/main/java/com/autobotix/controllers/CatgoryController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.autobotix.models.Category;
 import com.autobotix.models.Product;
 import com.autobotix.models.ResultDetails;
